a61ff7f61af6d8717eca1adbde3e11f8f3c2c35b
Florian Metz <me@timeraa.dev>
2025-04-06 17:39:09 +0200

    Refactor documentation for consistency and clarity
    - Updated JSON examples in localization, metadata, multilanguage, and settings guides to ensure consistent formatting and clarity.
    - Improved TypeScript code snippets across various guides, including presence, slideshow, and structure, for better readability and adherence to coding standards.
    - Removed unnecessary comments and adjusted spacing for improved visual structure.
    - Enhanced explanations and examples in the presence class and slideshow sections to provide clearer guidance on usage.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -13,6 +13,7 @@ const versions: VersionItem[] = [
 ]
 
 export default defineConfig({
+  base: 'Activities',
   title: 'PreMiD',
   description: 'Documentation for developing PreMiD Activities',
   themeConfig: {
